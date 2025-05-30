1bdeaebddd99c4f994f73402b135c29c2f4044da
Crisstaal <muyungacrys@gmail.com>
2025-05-30 09:02:20 -0500

    betty

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -22,11 +22,13 @@ typedef struct sig_s
 } sig_t;
 
 /* Function prototypes */
-uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]);
+uint8_t *sha256(int8_t const *s, size_t len,
+	uint8_t digest[SHA256_DIGEST_LENGTH]);
 EC_KEY *ec_create(void);
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
 int ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen, sig_t *sig);
-int ec_verify(EC_KEY const *key, uint8_t const *msg, size_t msglen, sig_t const *sig);
+int ec_verify(EC_KEY const *key, uint8_t const *msg,
+	size_t msglen, sig_t const *sig);
 
 #endif /* HBLK_CRYPTO_H */
--- crypto/sha256.c
+++ crypto/sha256.c
@@ -9,7 +9,8 @@
  *
  * Return: A pointer to digest, or NULL on failure
  */
-uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH])
+uint8_t *sha256(int8_t const *s, size_t len,
+	uint8_t digest[SHA256_DIGEST_LENGTH])
 {
 	if (!digest)
 		return (NULL);
