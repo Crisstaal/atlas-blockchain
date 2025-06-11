0e3a33bd4e5c4e3ac8637a58c3fbf6e9fc162b78
Crisstaal <muyungacrys@gmail.com>
2025-06-11 13:46:15 -0500

    betty

--- blockchain/v0.1/block_is_valid.c
+++ blockchain/v0.1/block_is_valid.c
@@ -4,18 +4,18 @@
 
 /* Define the genesis block for validation */
 static const block_t genesis_block = {
-    .info = {
+.info = {
 	.index = 0,
 	.difficulty = 0,
 	.timestamp = 1537578000,
 	.nonce = 0,
 	.prev_hash = {0},
-    },
-    .data = {
+},
+.data = {
 	.buffer = "Holberton School",
 	.len = 16,
-    },
-    .hash = {0xc5, 0x2c, 0x26, 0xc8, 0xb5, 0x46, 0x16, 0x39, 0x63, 0x5d, 0x8e, 0xdf, 0x2a, 0x97, 0xd4, 0x8d, 0x0c, 0x8e, 0x00, 0x09, 0xc8, 0x17, 0xf2, 0xb1, 0xd3, 0xd7, 0xff, 0x2f, 0x04, 0x51, 0x58, 0x03}};
+},
+.hash = {0xc5, 0x2c, 0x26, 0xc8, 0xb5, 0x46, 0x16, 0x39, 0x63, 0x5d, 0x8e, 0xdf, 0x2a, 0x97, 0xd4, 0x8d, 0x0c, 0x8e, 0x00, 0x09, 0xc8, 0x17, 0xf2, 0xb1, 0xd3, 0xd7, 0xff, 0x2f, 0x04, 0x51, 0x58, 0x03}};
 
 int block_is_valid(block_t const *block, block_t const *prev_block)
 {
@@ -41,7 +41,8 @@ int block_is_valid(block_t const *block, block_t const *prev_block)
 		if (block->data.len != genesis_block.data.len)
 			return (1);
 
-		if (memcmp(block->data.buffer, genesis_block.data.buffer, block->data.len) != 0)
+		if (memcmp(block->data.buffer, genesis_block.data.buffer,
+			block->data.len) != 0)
 			return (1);
 
 		/* Check hash */
@@ -63,7 +64,8 @@ int block_is_valid(block_t const *block, block_t const *prev_block)
 		return (1);
 
 	/* Check prev_hash matches prev_block's hash */
-	if (memcmp(block->info.prev_hash, prev_block->hash, SHA256_DIGEST_LENGTH) != 0)
+	if (memcmp(block->info.prev_hash, prev_block->hash,
+		SHA256_DIGEST_LENGTH) != 0)
 		return (1);
 
 	/* Check current block's hash */
