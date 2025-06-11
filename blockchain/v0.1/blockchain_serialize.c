51b2831b291dd8bf72a2ff27bf7b2542f9b8a49e
Crisstaal <muyungacrys@gmail.com>
2025-06-11 13:18:29 -0500

    trying last

--- blockchain/v0.1/blockchain_serialize.c
+++ blockchain/v0.1/blockchain_serialize.c
@@ -1,117 +1,159 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
-#include "blockchain.h"
-#include "llist.h"
-#include "endianness.h"
-#define HBLK_VERSION "0.1"
 
+/* Define your blockchain and block structs as needed */
+typedef struct block_s
+{
+	uint32_t index;
+	uint32_t difficulty;
+	uint64_t timestamp;
+	uint64_t nonce;
+	uint8_t prev_hash[32];
+	uint32_t data_len;
+	int8_t *data;
+	uint8_t hash[32];
+	struct block_s *next;
+} block_t;
 
-/**
- * swap_endian - Swap endianness of bytes in place
- * @value: Pointer to the value to swap
- * @size: Number of bytes
- */
-static void swap_endian(void *value, size_t size)
+typedef struct blockchain_s
+{
+	block_t *head;
+	size_t length;
+} blockchain_t;
+
+/* Helper to get system endianness
+ * Returns 1 for little endian, 2 for big endian */
+static uint8_t get_endianness(void)
+{
+	uint16_t test = 0x0001;
+	uint8_t *byte = (uint8_t *)&test;
+	return (byte[0] == 1) ? 1 : 2;
+}
+
+/* Helper to swap endianness in place */
+static void swap_endian(void *data, size_t size)
 {
 	size_t i;
-	uint8_t *ptr = value, tmp;
-
+	uint8_t *bytes = (uint8_t *)data;
 	for (i = 0; i < size / 2; i++)
 	{
-		tmp = ptr[i];
-		ptr[i] = ptr[size - 1 - i];
-		ptr[size - 1 - i] = tmp;
+		uint8_t tmp = bytes[i];
+		bytes[i] = bytes[size - 1 - i];
+		bytes[size - 1 - i] = tmp;
 	}
+}
+
+/* Write a 4-byte unsigned int in little or big endian depending on endianness */
+static int write_uint32(FILE *f, uint32_t val, uint8_t file_endian)
+{
+	if (file_endian != get_endianness())
+		swap_endian(&val, sizeof(val));
+	return fwrite(&val, sizeof(val), 1, f) == 1 ? 0 : -1;
+}
+
+/* Write an 8-byte unsigned int in little or big endian depending on endianness */
+static int write_uint64(FILE *f, uint64_t val, uint8_t file_endian)
+{
+	if (file_endian != get_endianness())
+		swap_endian(&val, sizeof(val));
+	return fwrite(&val, sizeof(val), 1, f) == 1 ? 0 : -1;
 }
 
 /**
  * blockchain_serialize - Serialize a blockchain into a file
- * @blockchain: Pointer to the blockchain to serialize
- * @path: File path to serialize into
+ * @blockchain: Pointer to blockchain to serialize
+ * @path: File path to write serialization
+ *
  * Return: 0 on success, -1 on failure
  */
 int blockchain_serialize(blockchain_t const *blockchain, char const *path)
 {
-	FILE *file;
-	uint8_t endian;
-	uint32_t blk_count, i;
+	FILE *f;
+	uint8_t magic[4] = {'H', 'B', 'L', 'K'};
+	char version[3] = {'0', '.', '1'}; /* version X.Y */
+	uint8_t endian = 1;		   /* 1 = little endian, 2 = big endian */
+	size_t i;
 	block_t *block;
 
 	if (!blockchain || !path)
-		return (-1);
+		return -1;
 
-	file = fopen(path, "wb");
-	if (!file)
-		return (-1);
+	/* Open file for writing (overwrite if exists) */
+	f = fopen(path, "wb");
+	if (!f)
+		return -1;
 
-	/* Write header magic */
-	if (fwrite("HBLK", 1, 4, file) != 4)
+	endian = get_endianness();
+
+	/* Write File Header */
+	if (fwrite(magic, sizeof(magic), 1, f) != 1)
+		goto fail;
+	if (fwrite(version, sizeof(version), 1, f) != 1)
+		goto fail;
+	if (fputc(endian, f) == EOF)
 		goto fail;
 
-	/* Write version */
-	if (fwrite(HBLK_VERSION, 1, 3, file) != 3)
+	/* Number of blocks (4 bytes) */
+	uint32_t num_blocks = (uint32_t)blockchain->length;
+	if (write_uint32(f, num_blocks, endian) < 0)
 		goto fail;
 
-	/* Write endianness */
-	endian = _get_endianness();
-	if (fwrite(&endian, 1, 1, file) != 1)
-		goto fail;
-
-	/* Write number of blocks */
-	blk_count = llist_size(blockchain->chain);
-	if (endian != _get_endianness())
-		swap_endian(&blk_count, sizeof(blk_count));
-	if (fwrite(&blk_count, sizeof(blk_count), 1, file) != 1)
-		goto fail;
-
-	/* Write each block */
-	for (i = 0; i < blk_count; i++)
+	/* Write blocks */
+	block = blockchain->head;
+	for (i = 0; i < blockchain->length; i++)
 	{
-		uint32_t index, difficulty, data_len;
-		uint64_t timestamp, nonce;
+		uint32_t tmp32;
+		uint64_t tmp64;
 
-		block = llist_get_node_at(blockchain->chain, i);
-		if (!block)
+		/* index */
+		tmp32 = block->index;
+		if (write_uint32(f, tmp32, endian) < 0)
 			goto fail;
 
-		index = block->info.index;
-		difficulty = block->info.difficulty;
-		timestamp = block->info.timestamp;
-		nonce = block->info.nonce;
-		data_len = block->data.len;
+		/* difficulty */
+		tmp32 = block->difficulty;
+		if (write_uint32(f, tmp32, endian) < 0)
+			goto fail;
 
-		if (endian != _get_endianness())
+		/* timestamp */
+		tmp64 = block->timestamp;
+		if (write_uint64(f, tmp64, endian) < 0)
+			goto fail;
+
+		/* nonce */
+		tmp64 = block->nonce;
+		if (write_uint64(f, tmp64, endian) < 0)
+			goto fail;
+
+		/* prev_hash */
+		if (fwrite(block->prev_hash, sizeof(block->prev_hash), 1, f) != 1)
+			goto fail;
+
+		/* data_len */
+		tmp32 = block->data_len;
+		if (write_uint32(f, tmp32, endian) < 0)
+			goto fail;
+
+		/* data */
+		if (block->data_len > 0)
 		{
-			swap_endian(&index, sizeof(index));
-			swap_endian(&difficulty, sizeof(difficulty));
-			swap_endian(&timestamp, sizeof(timestamp));
-			swap_endian(&nonce, sizeof(nonce));
-			swap_endian(&data_len, sizeof(data_len));
+			if (fwrite(block->data, block->data_len, 1, f) != 1)
+				goto fail;
 		}
 
-		if (fwrite(&index, sizeof(index), 1, file) != 1)
+		/* hash */
+		if (fwrite(block->hash, sizeof(block->hash), 1, f) != 1)
 			goto fail;
-		if (fwrite(&difficulty, sizeof(difficulty), 1, file) != 1)
-			goto fail;
-		if (fwrite(&timestamp, sizeof(timestamp), 1, file) != 1)
-			goto fail;
-		if (fwrite(&nonce, sizeof(nonce), 1, file) != 1)
-			goto fail;
-		if (fwrite(block->info.prev_hash, 1, 32, file) != 32)
-			goto fail;
-		if (fwrite(&data_len, sizeof(data_len), 1, file) != 1)
-			goto fail;
-		if (fwrite(block->data.buffer, 1, block->data.len, file) != block->data.len)
-			goto fail;
-		if (fwrite(block->hash, 1, 32, file) != 32)
-			goto fail;
+
+		block = block->next;
 	}
 
-	fclose(file);
-	return (0);
+	fclose(f);
+	return 0;
 
 fail:
-	fclose(file);
-	return (-1);
+	fclose(f);
+	return -1;
 }
