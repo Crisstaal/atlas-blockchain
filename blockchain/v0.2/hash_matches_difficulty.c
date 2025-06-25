42665e64e3056db631c142d677e65dfeb851919a
Crisstaal <muyungacrys@gmail.com>
2025-06-25 17:15:12 -0500

    compile

--- blockchain/v0.2/hash_matches_difficulty.c
+++ blockchain/v0.2/hash_matches_difficulty.c
@@ -11,7 +11,7 @@ int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
 			    uint32_t difficulty)
 {
 	uint32_t bits_checked = 0, i;
-	uint8_t mask, byte;
+	uint8_t byte;
 
 	for (i = 0; i < SHA256_DIGEST_LENGTH && bits_checked < difficulty; i++)
 	{
@@ -20,7 +20,7 @@ int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
 		for (int bit = 7; bit >= 0 && bits_checked < difficulty; bit--, bits_checked++)
 		{
 			if ((byte >> bit) & 1)
-				return (0); /* Found a '1' before reaching required difficulty */
+				return (0);
 		}
 	}
 
