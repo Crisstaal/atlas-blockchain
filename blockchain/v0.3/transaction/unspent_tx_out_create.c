50eb1748440f5e05b3fc8bad7e532a907e7b2b90
Crisstaal <muyungacrys@gmail.com>
2025-07-09 23:14:39 -0500

    try

--- blockchain/v0.3/transaction/unspent_tx_out_create.c
+++ blockchain/v0.3/transaction/unspent_tx_out_create.c
@@ -1,8 +1,7 @@
 #include "transaction.h"
-#include "blockchain.h"
-#include "hblk_crypto.h"
-#include "llist.h"
+#include "crypto/hblk_crypto.h" /* For SHA256_DIGEST_LENGTH */
 
+#include <stdlib.h>
 #include <string.h>
 
 /**
@@ -14,21 +13,21 @@
  * Return: pointer to new unspent_tx_out_t struct or NULL on failure
  */
 unspent_tx_out_t *unspent_tx_out_create(uint8_t block_hash[SHA256_DIGEST_LENGTH],
-					uint8_t tx_id[SHA256_DIGEST_LENGTH],
-					tx_out_t const *out)
+                                       uint8_t tx_id[SHA256_DIGEST_LENGTH],
+                                       tx_out_t const *out)
 {
-	unspent_tx_out_t *unspent;
+    unspent_tx_out_t *unspent;
 
-	if (!block_hash || !tx_id || !out)
-		return (NULL);
+    if (!block_hash || !tx_id || !out)
+        return NULL;
 
-	unspent = calloc(1, sizeof(unspent_tx_out_t));
-	if (!unspent)
-		return (NULL);
+    unspent = calloc(1, sizeof(unspent_tx_out_t));
+    if (!unspent)
+        return NULL;
 
-	memcpy(unspent->block_hash, block_hash, SHA256_DIGEST_LENGTH);
-	memcpy(unspent->tx_id, tx_id, SHA256_DIGEST_LENGTH);
-	memcpy(&(unspent->out), out, sizeof(tx_out_t));
+    memcpy(unspent->block_hash, block_hash, SHA256_DIGEST_LENGTH);
+    memcpy(unspent->tx_id, tx_id, SHA256_DIGEST_LENGTH);
+    memcpy(&(unspent->out), out, sizeof(tx_out_t));
 
-	return (unspent);
+    return unspent;
 }
