e5242b7480f389613b116ad89204d87339591894
Crisstaal <muyungacrys@gmail.com>
2025-07-17 10:49:40 -0500

    trying

--- blockchain/v0.3/blockchain_deserialize.c
+++ blockchain/v0.3/blockchain_deserialize.c
@@ -1,187 +1,205 @@
 #include "blockchain.h"
-#include "hblk_crypto.h"
-#include "transaction.h"
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>
 #include <unistd.h>
-
-#define HBLK_MAGIC "HBLK"
-#define HBLK_VERSION "0.3"
-#define HBLK_ENDIANNESS 1
-
-/**
- * read_uint32 - Reads a 4-byte unsigned int from a file descriptor (little endian)
- * @fd: File descriptor to read from
- * @val: Pointer to store the read uint32_t value
- *
- * Return: 0 on success, -1 on failure
- */
-static int read_uint32(int fd, uint32_t *val)
-{
-	uint8_t buf[4];
-	int i;
-
-	if (read(fd, buf, 4) != 4)
-		return (-1);
-
-	*val = 0;
-	for (i = 0; i < 4; i++)
-		*val |= (uint32_t)buf[i] << (8 * i);
-
-	return (0);
-}
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <stdio.h>
 
 /**
- * read_uint64 - Reads an 8-byte unsigned int from a file descriptor (little endian)
- * @fd: File descriptor to read from
- * @val: Pointer to store the read uint64_t value
- *
- * Return: 0 on success, -1 on failure
- */
-static int read_uint64(int fd, uint64_t *val)
-{
-	uint8_t buf[8];
-	int i;
-
-	if (read(fd, buf, 8) != 8)
-		return (-1);
-
-	*val = 0;
-	for (i = 0; i < 8; i++)
-		*val |= (uint64_t)buf[i] << (8 * i);
-
-	return (0);
-}
-
-/**
- * read_bytes - Reads len bytes from fd into buf
- * @fd: File descriptor to read from
- * @buf: Buffer to store read bytes
- * @len: Number of bytes to read
- *
- * Return: 0 on success, -1 on failure
+ * read_bytes - reads exactly `len` bytes from fd into buf
  */
 static int read_bytes(int fd, void *buf, size_t len)
 {
-	return (read(fd, buf, len) == (ssize_t)len ? 0 : -1);
-}
+	ssize_t rd, total = 0;
 
-/**
- * deserialize_tx_input - Deserialize a transaction input from fd
- * @fd: File descriptor to read from
- *
- * Return: Pointer to transaction_input_t on success, NULL on failure
- */
-static transaction_input_t *deserialize_tx_input(int fd)
-{
-	transaction_input_t *tx_in;
-
-	tx_in = malloc(sizeof(*tx_in));
-	if (!tx_in)
-		return (NULL);
-
-	if (read_bytes(fd, tx_in->block_hash, SHA256_DIGEST_LENGTH) == -1 ||
-	    read_bytes(fd, tx_in->tx_id, SHA256_DIGEST_LENGTH) == -1 ||
-	    read_bytes(fd, tx_in->tx_out_hash, SHA256_DIGEST_LENGTH) == -1 ||
-	    read_bytes(fd, tx_in->sig.sig, 72) == -1 ||
-	    read_bytes(fd, &tx_in->sig.len, 1) == -1)
+	while (len > 0 && (rd = read(fd, buf, len)) > 0)
 	{
-		free(tx_in);
-		return (NULL);
+		total += rd;
+		buf = (char *)buf + rd;
+		len -= rd;
 	}
-
-	return (tx_in);
+	return (rd < 0 || len > 0) ? -1 : 0;
 }
 
 /**
- * deserialize_tx_output - Deserialize a transaction output from fd
- * @fd: File descriptor to read from
- *
- * Return: Pointer to transaction_output_t on success, NULL on failure
+ * read_uint32 - read 4 bytes as uint32_t
  */
-static transaction_output_t *deserialize_tx_output(int fd)
+static int read_uint32(int fd, uint32_t *n)
 {
-	transaction_output_t *tx_out;
-
-	tx_out = malloc(sizeof(*tx_out));
-	if (!tx_out)
-		return (NULL);
-
-	if (read_uint32(fd, &tx_out->amount) == -1 ||
-	    read_bytes(fd, tx_out->pub, EC_PUB_LEN) == -1 ||
-	    read_bytes(fd, tx_out->hash, SHA256_DIGEST_LENGTH) == -1)
-	{
-		free(tx_out);
-		return (NULL);
-	}
-
-	return (tx_out);
+	return read_bytes(fd, n, sizeof(*n));
 }
 
 /**
- * deserialize_transaction - Deserialize a transaction from fd
- * @fd: File descriptor to read from
- *
- * Return: Pointer to transaction_t on success, NULL on failure
+ * read_uint64 - read 8 bytes as uint64_t
+ */
+static int read_uint64(int fd, uint64_t *n)
+{
+	return read_bytes(fd, n, sizeof(*n));
+}
+
+/**
+ * read_buffer - reads an exact-length buffer from file
+ */
+static int read_buffer(int fd, void *buf, size_t size)
+{
+	return read_bytes(fd, buf, size);
+}
+
+/**
+ * deserialize_tx_input - reads a transaction input from file
+ */
+static tx_in_t *deserialize_tx_input(int fd)
+{
+	tx_in_t *in = calloc(1, sizeof(*in));
+	if (!in)
+		return NULL;
+
+	if (read_buffer(fd, in->block_hash, SHA256_DIGEST_LENGTH) < 0 ||
+	    read_buffer(fd, in->tx_id, SHA256_DIGEST_LENGTH) < 0 ||
+	    read_uint32(fd, &in->tx_out_hash_len) < 0 ||
+	    read_buffer(fd, in->sig, sizeof(in->sig)) < 0 ||
+	    read_uint32(fd, &in->sig_len) < 0)
+	{
+		free(in);
+		return NULL;
+	}
+	return in;
+}
+
+/**
+ * deserialize_tx_output - reads a transaction output from file
+ */
+static tx_out_t *deserialize_tx_output(int fd)
+{
+	tx_out_t *out = calloc(1, sizeof(*out));
+	if (!out)
+		return NULL;
+
+	if (read_uint32(fd, &out->amount) < 0 ||
+	    read_buffer(fd, out->pub, EC_PUB_LEN) < 0 ||
+	    read_buffer(fd, out->hash, SHA256_DIGEST_LENGTH) < 0)
+	{
+		free(out);
+		return NULL;
+	}
+	return out;
+}
+
+/**
+ * deserialize_transaction - reads a transaction from file
  */
 static transaction_t *deserialize_transaction(int fd)
 {
-	transaction_t *tx;
-	uint32_t nb_inputs, nb_outputs;
-	uint32_t i;
+	transaction_t *tx = calloc(1, sizeof(*tx));
+	uint32_t count, i;
 
-	tx = malloc(sizeof(*tx));
 	if (!tx)
-		return (NULL);
+		return NULL;
 
-	if (read_bytes(fd, tx->id, SHA256_DIGEST_LENGTH) == -1)
+	if (read_buffer(fd, tx->id, SHA256_DIGEST_LENGTH) < 0)
 		goto fail;
 
-	if (read_uint32(fd, &nb_inputs) == -1)
+	if (read_uint32(fd, &count) < 0)
 		goto fail;
-
-	if (read_uint32(fd, &nb_outputs) == -1)
-		goto fail;
-
-	tx->inputs = llist_create(MTXFREE);
-	tx->outputs = llist_create(MTXFREE);
-
-	if (!tx->inputs || !tx->outputs)
-		goto fail;
-
-	for (i = 0; i < nb_inputs; i++)
+	tx->inputs = llist_create(MT_SUPPORT_TRUE);
+	for (i = 0; i < count; i++)
 	{
-		transaction_input_t *tx_in = deserialize_tx_input(fd);
-
-		if (!tx_in)
+		tx_in_t *in = deserialize_tx_input(fd);
+		if (!in || llist_add_node(tx->inputs, in, ADD_NODE_REAR) < 0)
 			goto fail;
-
-		llist_add_node(tx->inputs, tx_in, ADD_NODE_REAR);
 	}
 
-	for (i = 0; i < nb_outputs; i++)
+	if (read_uint32(fd, &count) < 0)
+		goto fail;
+	tx->outputs = llist_create(MT_SUPPORT_TRUE);
+	for (i = 0; i < count; i++)
 	{
-		transaction_output_t *tx_out = deserialize_tx_output(fd);
-
-		if (!tx_out)
+		tx_out_t *out = deserialize_tx_output(fd);
+		if (!out || llist_add_node(tx->outputs, out, ADD_NODE_REAR) < 0)
 			goto fail;
-
-		llist_add_node(tx->outputs, tx_out, ADD_NODE_REAR);
 	}
-
-	return (tx);
+	return tx;
 
 fail:
-	if (tx)
+	if (tx->inputs)
+		llist_destroy(tx->inputs, 1, free);
+	if (tx->outputs)
+		llist_destroy(tx->outputs, 1, free);
+	free(tx);
+	return NULL;
+}
+
+/**
+ * blockchain_deserialize - rebuilds blockchain from file
+ */
+blockchain_t *blockchain_deserialize(char const *path)
+{
+	int fd;
+	uint8_t magic[4], version[3];
+	uint8_t endian;
+	uint32_t blocks, i, txs, j;
+	blockchain_t *bc;
+	block_t *block;
+	block_info_t info;
+	block_data_t data;
+
+	fd = open(path, O_RDONLY);
+	if (fd < 0)
+		return NULL;
+
+	if (read_buffer(fd, magic, 4) < 0 || memcmp(magic, "HBLK", 4) != 0 ||
+	    read_buffer(fd, version, 3) < 0 ||
+	    read_buffer(fd, &endian, 1) < 0 ||
+	    read_uint32(fd, &blocks) < 0)
 	{
-		if (tx->inputs)
-			llist_destroy(tx->inputs, 1);
-		if (tx->outputs)
-			llist_destroy(tx->outputs, 1);
-		free(tx);
+		close(fd);
+		return NULL;
 	}
 
-	return (NULL);
+	bc = calloc(1, sizeof(*bc));
+	if (!bc)
+		return close(fd), NULL;
+
+	bc->chain = llist_create(MT_SUPPORT_TRUE);
+	for (i = 0; i < blocks; i++)
+	{
+		if (read_buffer(fd, &info, sizeof(info)) < 0 ||
+		    read_uint32(fd, &data.len) < 0 ||
+		    read_buffer(fd, data.buffer, data.len) < 0 ||
+		    read_buffer(fd, data.hash, SHA256_DIGEST_LENGTH) < 0 ||
+		    read_uint32(fd, &txs) < 0)
+			goto fail;
+
+		block = calloc(1, sizeof(*block));
+		if (!block)
+			goto fail;
+
+		memcpy(&block->info, &info, sizeof(info));
+		memcpy(&block->data, &data, sizeof(data));
+
+		block->transactions = llist_create(MT_SUPPORT_TRUE);
+		for (j = 0; j < txs; j++)
+		{
+			transaction_t *tx = deserialize_transaction(fd);
+			if (!tx || llist_add_node(block->transactions, tx, ADD_NODE_REAR) < 0)
+				goto fail;
+		}
+
+		if (llist_add_node(bc->chain, block, ADD_NODE_REAR) < 0)
+			goto fail;
+	}
+
+	close(fd);
+	return bc;
+
+fail:
+	if (bc)
+	{
+		if (bc->chain)
+			llist_destroy(bc->chain, 1, free);
+		free(bc);
+	}
+	close(fd);
+	return NULL;
 }
--- blockchain/v0.3/blockchain_serialize.c
+++ blockchain/v0.3/blockchain_serialize.c
@@ -1,222 +1,187 @@
 #include "blockchain.h"
-#include "llist.h"
-#include "transaction.h"
-#include "hblk_crypto.h"
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>  /* for close() */
-#include <fcntl.h>   /* for open() */
-#include <errno.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
 #define HBLK_MAGIC "HBLK"
 #define HBLK_VERSION "0.3"
-#define HBLK_ENDIANNESS 1 /* little endian */
 
-/**
- * write_uint32 - Write a 4-byte unsigned int (little endian) to fd
- * @fd: File descriptor to write to
- * @val: uint32_t value to write
- *
- * Return: 0 on success, -1 on failure
- */
-static int write_uint32(int fd, uint32_t val)
+static int write_bytes(FILE *f, void *ptr, size_t size)
 {
-	uint8_t buf[4];
-
-	buf[0] = val & 0xFF;
-	buf[1] = (val >> 8) & 0xFF;
-	buf[2] = (val >> 16) & 0xFF;
-	buf[3] = (val >> 24) & 0xFF;
-
-	return (write(fd, buf, 4) == 4 ? 0 : -1);
+	return fwrite(ptr, size, 1, f) == 1 ? 0 : -1;
 }
 
-/**
- * write_uint64 - Write an 8-byte unsigned int (little endian) to fd
- * @fd: File descriptor to write to
- * @val: uint64_t value to write
- *
- * Return: 0 on success, -1 on failure
- */
-static int write_uint64(int fd, uint64_t val)
+static int write_uint32(FILE *f, uint32_t val)
 {
-	uint8_t buf[8];
-	int i;
-
-	for (i = 0; i < 8; i++)
-		buf[i] = (val >> (8 * i)) & 0xFF;
-
-	return (write(fd, buf, 8) == 8 ? 0 : -1);
+	uint32_t net_val = htonl(val);
+	return write_bytes(f, &net_val, sizeof(net_val));
 }
 
-/**
- * write_bytes - Write len bytes from buf to fd
- * @fd: File descriptor to write to
- * @buf: Buffer containing data to write
- * @len: Number of bytes to write
- *
- * Return: 0 on success, -1 on failure
- */
-static int write_bytes(int fd, void const *buf, size_t len)
+static int write_uint64(FILE *f, uint64_t val)
 {
-	return (write(fd, buf, len) == (ssize_t)len ? 0 : -1);
+	uint64_t net_val = htobe64(val);
+	return write_bytes(f, &net_val, sizeof(net_val));
 }
 
-/**
- * serialize_transaction - Serialize a transaction to fd
- * @fd: File descriptor to write to
- * @tx: Pointer to the transaction to serialize
- *
- * Return: 0 on success, -1 on failure
- */
-static int serialize_transaction(int fd, transaction_t const *tx)
+static int serialize_tx_in(FILE *f, tx_in_t *in)
 {
-	uint32_t nb_inputs, nb_outputs;
-	llist_node_t *node;
-	tx_in_t *input;
-	tx_out_t *output;
+	if (!in)
+		return -1;
 
-	if (!tx)
-		return (-1);
+	if (fwrite(in->block_hash, 1, SHA256_DIGEST_LENGTH, f) != SHA256_DIGEST_LENGTH ||
+	    fwrite(in->tx_id, 1, SHA256_DIGEST_LENGTH, f) != SHA256_DIGEST_LENGTH ||
+	    fwrite(in->tx_out_hash, 1, SHA256_DIGEST_LENGTH, f) != SHA256_DIGEST_LENGTH ||
+	    fwrite(in->sig.sig, 1, sizeof(in->sig.sig), f) != sizeof(in->sig.sig) ||
+	    fwrite(&in->sig.len, 1, 1, f) != 1)
+		return -1;
 
-	nb_inputs = llist_size(tx->inputs);
-	nb_outputs = llist_size(tx->outputs);
+	return 0;
+}
 
-	if (write_bytes(fd, tx->id, SHA256_DIGEST_LENGTH) == -1)
-		return (-1);
+static int serialize_tx_out(FILE *f, tx_out_t *out)
+{
+	if (!out)
+		return -1;
 
-	if (write_uint32(fd, nb_inputs) == -1)
-		return (-1);
+	if (write_uint32(f, out->amount) < 0 ||
+	    fwrite(out->pub, 1, EC_PUB_LEN, f) != EC_PUB_LEN ||
+	    fwrite(out->hash, 1, SHA256_DIGEST_LENGTH, f) != SHA256_DIGEST_LENGTH)
+		return -1;
 
-	if (write_uint32(fd, nb_outputs) == -1)
-		return (-1);
+	return 0;
+}
 
-	/* Write each input */
-	node = tx->inputs->head;
-	while (node)
+static int serialize_transaction(FILE *f, transaction_t *tx)
+{
+	uint32_t n_inputs, n_outputs;
+	size_t i;
+
+	if (!tx || !f)
+		return -1;
+
+	if (fwrite(tx->id, 1, SHA256_DIGEST_LENGTH, f) != SHA256_DIGEST_LENGTH)
+		return -1;
+
+	n_inputs = llist_size(tx->inputs);
+	n_outputs = llist_size(tx->outputs);
+
+	if (write_uint32(f, n_inputs) < 0 || write_uint32(f, n_outputs) < 0)
+		return -1;
+
+	for (i = 0; i < n_inputs; i++)
 	{
-		input = (tx_in_t *)node->data;
-		if (write_bytes(fd, input->block_hash, SHA256_DIGEST_LENGTH) == -1 ||
-		    write_bytes(fd, input->tx_id, SHA256_DIGEST_LENGTH) == -1 ||
-		    write_bytes(fd, input->tx_out_hash, SHA256_DIGEST_LENGTH) == -1 ||
-		    write_bytes(fd, input->sig.sig, 72) == -1 ||
-		    write_bytes(fd, &input->sig.len, 1) == -1)
-			return (-1);
-		node = node->next;
+		tx_in_t *in = llist_get_node_at(tx->inputs, i);
+		if (serialize_tx_in(f, in) < 0)
+			return -1;
 	}
 
-	/* Write each output */
-	node = tx->outputs->head;
-	while (node)
+	for (i = 0; i < n_outputs; i++)
 	{
-		output = (tx_out_t *)node->data;
-		if (write_uint32(fd, output->amount) == -1 ||
-		    write_bytes(fd, output->pub, EC_PUB_LEN) == -1 ||
-		    write_bytes(fd, output->hash, SHA256_DIGEST_LENGTH) == -1)
-			return (-1);
-		node = node->next;
+		tx_out_t *out = llist_get_node_at(tx->outputs, i);
+		if (serialize_tx_out(f, out) < 0)
+			return -1;
 	}
 
-	return (0);
+	return 0;
 }
 
-/**
- * blockchain_serialize - Serialize a blockchain to a file
- * @blockchain: Pointer to blockchain to serialize
- * @path: Path to file to write serialized data
- *
- * Return: 0 on success, -1 on failure
- */
+static int serialize_block(FILE *f, block_t *block)
+{
+	uint32_t nb_tx;
+	size_t i;
+
+	if (!block || !f)
+		return -1;
+
+	if (write_uint32(f, block->info.index) < 0 ||
+	    write_uint32(f, block->info.difficulty) < 0 ||
+	    write_uint64(f, block->info.timestamp) < 0 ||
+	    write_uint64(f, block->info.nonce) < 0 ||
+	    fwrite(block->info.prev_hash, 1, SHA256_DIGEST_LENGTH, f) != SHA256_DIGEST_LENGTH ||
+	    write_uint32(f, block->data.len) < 0 ||
+	    fwrite(block->data.buffer, 1, block->data.len, f) != block->data.len ||
+	    fwrite(block->hash, 1, SHA256_DIGEST_LENGTH, f) != SHA256_DIGEST_LENGTH)
+		return -1;
+
+	if (block->transactions == NULL)
+		return write_uint32(f, -1);
+
+	nb_tx = llist_size(block->transactions);
+	if (write_uint32(f, nb_tx) < 0)
+		return -1;
+
+	for (i = 0; i < nb_tx; i++)
+	{
+		transaction_t *tx = llist_get_node_at(block->transactions, i);
+		if (serialize_transaction(f, tx) < 0)
+			return -1;
+	}
+
+	return 0;
+}
+
+static int serialize_unspent(FILE *f, unspent_tx_out_t *utxo)
+{
+	if (!utxo)
+		return -1;
+
+	if (fwrite(utxo->block_hash, 1, SHA256_DIGEST_LENGTH, f) != SHA256_DIGEST_LENGTH ||
+	    fwrite(utxo->tx_id, 1, SHA256_DIGEST_LENGTH, f) != SHA256_DIGEST_LENGTH ||
+	    write_uint32(f, utxo->out.amount) < 0 ||
+	    fwrite(utxo->out.pub, 1, EC_PUB_LEN, f) != EC_PUB_LEN ||
+	    fwrite(utxo->out.hash, 1, SHA256_DIGEST_LENGTH, f) != SHA256_DIGEST_LENGTH)
+		return -1;
+
+	return 0;
+}
+
 int blockchain_serialize(blockchain_t const *blockchain, char const *path)
 {
-	int fd;
-	uint32_t nb_blocks, nb_unspent;
-	llist_node_t *node;
-	block_t *block;
-	uint32_t nb_tx;
-	llist_node_t *tx_node;
-	transaction_t *tx;
-	unspent_tx_out_t *uout;
+	FILE *f;
+	uint32_t n_blocks, n_unspent;
+	size_t i;
 
 	if (!blockchain || !path)
-		return (-1);
+		return -1;
 
-	fd = open(path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd == -1)
-		return (-1);
+	f = fopen(path, "wb");
+	if (!f)
+		return -1;
 
-	/* Write file header */
-	if (write_bytes(fd, HBLK_MAGIC, 4) == -1 ||
-	    write_bytes(fd, HBLK_VERSION, 3) == -1)
-		goto fail;
+	fwrite(HBLK_MAGIC, 1, 4, f);
+	fwrite(HBLK_VERSION, 1, 3, f);
+	fputc(1, f); /* Little endian */
 
+	n_blocks = llist_size(blockchain->chain);
+	n_unspent = llist_size(blockchain->unspent);
+
+	if (write_uint32(f, n_blocks) < 0 || write_uint32(f, n_unspent) < 0)
 	{
-		uint8_t endian = HBLK_ENDIANNESS;
-
-		if (write_bytes(fd, &endian, 1) == -1)
-			goto fail;
+		fclose(f);
+		return -1;
 	}
 
-	nb_blocks = llist_size(blockchain->chain);
-	nb_unspent = llist_size(blockchain->unspent);
-
-	if (write_uint32(fd, nb_blocks) == -1 ||
-	    write_uint32(fd, nb_unspent) == -1)
-		goto fail;
-
-	/* Serialize blocks */
-	node = blockchain->chain->head;
-	while (node)
+	for (i = 0; i < n_blocks; i++)
 	{
-		block = (block_t *)node->data;
-
-		if (write_uint32(fd, block->info.index) == -1 ||
-		    write_uint32(fd, block->info.difficulty) == -1 ||
-		    write_uint64(fd, block->info.timestamp) == -1 ||
-		    write_uint64(fd, block->info.nonce) == -1 ||
-		    write_bytes(fd, block->info.prev_hash, SHA256_DIGEST_LENGTH) == -1 ||
-		    write_uint32(fd, block->data.len) == -1 ||
-		    write_bytes(fd, block->data.buffer, block->data.len) == -1 ||
-		    write_bytes(fd, block->hash, SHA256_DIGEST_LENGTH) == -1)
-			goto fail;
-
-		nb_tx = 0;
-		if (block->transactions)
-			nb_tx = llist_size(block->transactions);
-
-		if (write_uint32(fd, nb_tx) == -1)
-			goto fail;
-
-		tx_node = block->transactions ? block->transactions->head : NULL;
-		while (tx_node)
+		block_t *blk = llist_get_node_at(blockchain->chain, i);
+		if (serialize_block(f, blk) < 0)
 		{
-			tx = (transaction_t *)tx_node->data;
-			if (serialize_transaction(fd, tx) == -1)
-				goto fail;
-			tx_node = tx_node->next;
+			fclose(f);
+			return -1;
 		}
-		node = node->next;
 	}
 
-	/* Serialize unspent transaction outputs */
-	node = blockchain->unspent->head;
-	while (node)
+	for (i = 0; i < n_unspent; i++)
 	{
-		uout = (unspent_tx_out_t *)node->data;
-		if (write_bytes(fd, uout->block_hash, SHA256_DIGEST_LENGTH) == -1 ||
-		    write_bytes(fd, uout->tx_id, SHA256_DIGEST_LENGTH) == -1 ||
-		    write_uint32(fd, uout->out.amount) == -1 ||
-		    write_bytes(fd, uout->out.pub, EC_PUB_LEN) == -1 ||
-		    write_bytes(fd, uout->out.hash, SHA256_DIGEST_LENGTH) == -1)
-			goto fail;
-		node = node->next;
+		unspent_tx_out_t *utxo = llist_get_node_at(blockchain->unspent, i);
+		if (serialize_unspent(f, utxo) < 0)
+		{
+			fclose(f);
+			return -1;
+		}
 	}
 
-	close(fd);
-	return (0);
-
-fail:
-	close(fd);
-	return (-1);
+	fclose(f);
+	return 0;
 }
