219813c984b02d048267a94d0418006fadb27448
Crisstaal <muyungacrys@gmail.com>
2025-07-16 20:28:04 -0500

    save hash

--- blockchain/v0.3/transaction/transaction_hash.c
+++ blockchain/v0.3/transaction/transaction_hash.c
@@ -8,11 +8,11 @@
  *
  * Return: pointer to hash_buf, or NULL on failure
  */
-uint8_t *transaction_hash(transaction_t const *transaction,
+uint8_t *transaction_hash(const transaction_t *transaction,
 			  uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-	size_t i, buffer_len = 0, num_inputs, num_outputs;
-	uint8_t *buffer, *cursor;
+	size_t i, buffer_len, num_inputs, num_outputs;
+	uint8_t *buffer = NULL, *cursor;
 	tx_in_t *in;
 	tx_out_t *out;
 
@@ -22,7 +22,11 @@ uint8_t *transaction_hash(transaction_t const *transaction,
 	num_inputs = llist_size(transaction->inputs);
 	num_outputs = llist_size(transaction->outputs);
 
-	buffer_len = num_inputs * (3 * SHA256_DIGEST_LENGTH) +
+	/* Defensive check for llist_size failure (-1 cast to size_t = huge number) */
+	if (num_inputs == (size_t)-1 || num_outputs == (size_t)-1)
+		return (NULL);
+
+	buffer_len = num_inputs * 3 * SHA256_DIGEST_LENGTH +
 		     num_outputs * SHA256_DIGEST_LENGTH;
 
 	buffer = calloc(1, buffer_len);
@@ -31,29 +35,36 @@ uint8_t *transaction_hash(transaction_t const *transaction,
 
 	cursor = buffer;
 
+	/* Append all input hashes */
 	for (i = 0; i < num_inputs; i++)
 	{
 		in = llist_get_node_at(transaction->inputs, i);
 		if (!in)
 			goto cleanup;
+
 		memcpy(cursor, in->block_hash, SHA256_DIGEST_LENGTH);
 		cursor += SHA256_DIGEST_LENGTH;
+
 		memcpy(cursor, in->tx_id, SHA256_DIGEST_LENGTH);
 		cursor += SHA256_DIGEST_LENGTH;
+
 		memcpy(cursor, in->tx_out_hash, SHA256_DIGEST_LENGTH);
 		cursor += SHA256_DIGEST_LENGTH;
 	}
 
+	/* Append all output hashes */
 	for (i = 0; i < num_outputs; i++)
 	{
 		out = llist_get_node_at(transaction->outputs, i);
 		if (!out)
 			goto cleanup;
+
 		memcpy(cursor, out->hash, SHA256_DIGEST_LENGTH);
 		cursor += SHA256_DIGEST_LENGTH;
 	}
 
-	sha256(buffer, buffer_len, hash_buf);
+	/* Compute the hash */
+	sha256((const int8_t *)buffer, buffer_len, hash_buf);
 	free(buffer);
 	return (hash_buf);
 
