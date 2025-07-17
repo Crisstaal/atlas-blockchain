4334805d6285aa16e75aa822a0511aeb5703ea49
Crisstaal <muyungacrys@gmail.com>
2025-07-17 09:07:08 -0500

    clean .h

--- blockchain/v0.3/transaction/transaction.h
+++ blockchain/v0.3/transaction/transaction.h
@@ -1,18 +1,14 @@
 #ifndef TRANSACTION_H
 #define TRANSACTION_H
 
-#include "crypto/hblk_crypto.h" 
-
 #include "blockchain.h"
+#include "crypto/hblk_crypto.h" 
 #include <stddef.h>
 #include <stdint.h>
 #include <openssl/ec.h>
 #include <openssl/sha.h>
 
 #include "llist.h"
-
-#define EC_PUB_LEN 65   
-#define COINBASE_AMOUNT 50
 
 typedef struct blockchain_s blockchain_t;
 
@@ -31,44 +27,7 @@ typedef struct transaction_s
     llist_t *outputs;
 } transaction_t;
 
-/**
- * struct tx_in_s - transaction input
- * @block_hash: hash of the block containing the referenced output
- * @tx_id: hash of the transaction containing the referenced output
- * @tx_out_hash: hash of the referenced output
- * @sig: signature of the transaction
- */
-typedef struct tx_in_s
-{
-    uint8_t block_hash[SHA256_DIGEST_LENGTH];
-    uint8_t tx_id[SHA256_DIGEST_LENGTH];
-    uint8_t tx_out_hash[SHA256_DIGEST_LENGTH];
-    sig_t sig;
-} tx_in_t;
-
-/**
- * struct tx_out_s - transaction output
- * @amount: amount from transaction
- * @pub: recipient's public key
- * @hash: hash of this output (for reference)
- */
-typedef struct tx_out_s
-{
-    uint32_t amount;
-    uint8_t pub[EC_PUB_LEN];
-    uint8_t hash[SHA256_DIGEST_LENGTH];
-} tx_out_t;
-
-
 /* Function prototypes */
-
-unspent_tx_out_t *unspent_tx_out_create(
-    uint8_t block_hash[SHA256_DIGEST_LENGTH],
-    uint8_t tx_id[SHA256_DIGEST_LENGTH],
-    tx_out_t const *out);
-
-int blockchain_serialize(blockchain_t const *blockchain, char const *path);
-blockchain_t *blockchain_deserialize(char const *path);
 
 tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN]);
 tx_in_t *tx_in_create(unspent_tx_out_t const *unspent);
